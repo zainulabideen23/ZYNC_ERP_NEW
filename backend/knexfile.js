9cae60b946ddb2d559e9841ed3bb822162fc37f2
Developer <developer@zyncerp.com>
2026-05-21 09:37:31 +0500

    fix: add seeds directory to knex production config

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -61,6 +61,9 @@ module.exports = {
     migrations: {
       directory: './database/migrations',
       tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
     }
   }
 };
